3a221c65f2bb90b30ad17d6267a53656daf2a232
Prabhav Thali <Prabhav.Thali1@ibm.com>
2020-09-18 16:36:42 +0530

    graphql trial query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Inside index.js");
+// Import graphql server
+import { GraphQLServer } from 'graphql-yoga';
+
+// Schema type defs
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+//Type resolvers
+const resolvers = {
+    Query : {
+        hello() {
+            return 'trial query!';
+        }
+    }
+}
+
+// Creating server definition
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+// Start the server
+server.start( ({port}) => {
+    console.log('Graphql server is now running on ${port}');
+})
+
+//console.log("Inside index.js");
